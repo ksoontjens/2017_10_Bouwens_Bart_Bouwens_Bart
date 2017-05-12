186710875639a55e6d9835dab0c14163abe2bd1c
Cyretti <thomas.vanvelk@telenet.be>
2017-05-12 11:05:15 +0200

    full restart

---
+++ JavaApplication5/src/javaapplication5/NewXlet.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication5;
+
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+
+/**
+ * 
+ * @author student
+ */
+public class NewXlet implements Xlet {
+
+    public void initXlet(XletContext context) {
+    }
+    
+    public void startXlet() {
+    }
+    
+    public void pauseXlet() {
+    }
+    
+    public void destroyXlet(boolean unconditional) {
+    }
+
+}
---
+++ Testenviroment/src/NewXlet1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+
+/**
+ * 
+ * @author student
+ */
+public class NewXlet1 implements Xlet {
+
+    public void initXlet(XletContext context) {
+    }
+    
+    public void startXlet() {
+    }
+    
+    public void pauseXlet() {
+    }
+    
+    public void destroyXlet(boolean unconditional) {
+    }
+
+}
